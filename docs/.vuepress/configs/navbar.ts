93402a9155a5d81105b06aa8e32ffd49466195b5
tierlabx <1765080897@qq.com>
2026-09-11 11:05:40 +0800

    feat: 重新组织全站的导航栏与侧边栏，构建系统化学习路径

--- docs/.vuepress/configs/navbar.ts
+++ docs/.vuepress/configs/navbar.ts
@@ -1,190 +1,98 @@
 export default {
   zh: [
     {
-      text: "Javascript",
+      text: "🧭 导航与指南",
+      children: [
+        "/guide/index.md",
+        "/guide/start.md",
+      ],
+    },
+    {
+      text: "🖥️ 前端开发",
       children: [
         {
-          text: "Javascript",
+          text: "JavaScript",
           children: [
-            {
-              text: "JS基础",
-              link: "/JS/基础/JS基础语法.md",
-            },
-            {
-              text: "算法",
-              link: "/JS/算法/index.md",
-            },
+            "/JS/基础/JS基础语法.md",
+            "/JS/算法/index.md",
           ],
         },
         {
-          text: "微信小程序",
-          children: ["/JS/微信小程序/index.md", "/JS/微信小程序/share.md"],
+          text: "Vue 生态",
+          children: [
+            "/vue/vuex/index.md",
+            "/vue/route/index.md",
+          ],
         },
         {
-          text: "UNAPP",
-          children: ["/JS/uniapp/index.md"],
+          text: "React",
+          link: "/react/index.md",
         },
         {
-          text: "自适应",
-          children: ["/JS/PC/rem.md"],
-        },
-      ],
-    },
-    {
-      text: "THREE",
-      children: [
-        {
-          text: "模型加载",
-          link: "/THREE/模型加载.md",
-        },
-        {
-          text: "射线检测",
-          link: "/THREE/射线检测.md",
+          text: "CSS / SCSS",
+          link: "/scss/index.md",
         },
       ],
     },
     {
-      text: "计算机基础",
+      text: "📱 跨端与图形",
       children: [
         {
-          text: "计算机组成",
+          text: "小程序 & UniApp",
+          children: [
+            "/JS/微信小程序/index.md",
+            "/JS/uniapp/index.md",
+          ],
+        },
+        {
+          text: "Flutter",
+          link: "/flutter/flutter第一天.md",
+        },
+        {
+          text: "Three.js",
+          children: [
+            "/THREE/模型加载.md",
+            "/THREE/射线检测.md",
+          ],
+        },
+      ],
+    },
+    {
+      text: "⚙️ 后端与底层",
+      children: [
+        {
+          text: "Golang",
+          link: "/Golang/datatype/hellow.md",
+        },
+        {
+          text: "Rust",
+          link: "/rust/index.md",
+        },
+        {
+          text: "计算机基础",
           link: "/computer/计算机基础.md",
         },
       ],
     },
     {
-      text: "Vue",
+      text: "🛠️ 工具与面经",
       children: [
         {
-          text: "Vue",
+          text: "开发工具",
           children: [
-            {
-              text: "vue的优缺点",
-              link: "/vue/vuex/you.md",
-            },
-            {
-              text: "vuex",
-              link: "/vue/vuex/index.md",
-            },
-
-            {
-              text: "Vue过滤器",
-              link: "/vue/vue/filters.md",
-            },
-            {
-              text: "v-model",
-              link: "/vue/vue/v-model.md",
-            },
+            { text: "Emacs 指南", link: "/emacs/emacs基本操作.md" },
+            { text: "Git 问题", link: "/fqa/git.md" },
+            { text: "NPM 常见", link: "/fqa/npm/npm.md" },
           ],
         },
         {
-          text: "插件",
-          children: [
-            {
-              text: "开发vue插件",
-              link: "/vue/插件/插件.md",
-            },
-            {
-              text: "vue-baidu-map离线地图",
-              link: "/vue/插件/离线地图.md",
-            },
-            {
-              text: "在vue中使用mockjs",
-              link: "/vue/插件/mock.md",
-            },
-          ],
-        },
-        {
-          text: "Layout",
-          children: [
-            {
-              text: "多布局切换",
-              link: "/vue/layout/index.md",
-            },
-          ],
+          text: "面试宝典",
+          link: "/fqa/interview/前端面试复习计划.md",
         },
       ],
     },
     {
-      text: "SCSS",
-      children: [
-        {
-          text: "SCSS",
-          children: [{ text: "SCSS", link: "/scss/index.md" }],
-        },
-      ],
-    },
-    {
-      text: "React",
-      children: [
-        {
-          text: "React",
-          children: [{ text: "react", link: "/react/index.md" }],
-        },
-      ],
-    },
-
-    {
-      text: "Golang",
-      children: [
-        {
-          text: "go基础",
-          children: [
-            {
-              text: "hellow word",
-              link: "/Golang/datatype/hellow.md",
-            },
-            {
-              text: "基础知识",
-              link: "/Golang/datatype/GO语言的主要特征.md",
-            },
-          ],
-        },
-        {
-          text: "数据库",
-          children: [
-            {
-              text: "GORM",
-              link: "/Golang/datasource/datasource.md",
-            },
-          ],
-        },
-      ],
-    },
-    {
-      text: "FQA",
-      children: [
-        {
-          text: "npm",
-          children: [
-            {
-              text: "npm",
-              link: "/fqa/npm/npm.md",
-            },
-          ],
-        },
-        {
-          text: "vue",
-          children: [
-            {
-              text: "vue",
-              link: "/fqa/vue/vue.md",
-            },
-          ],
-        },
-        {
-          text: "面试",
-          children: [
-            {
-              text: "前端面试复习计划",
-              link: "/fqa/interview/前端面试复习计划.md",
-            },
-          ],
-        },
-      ],
-    },
-    {
-      text: "链接",
+      text: "🔗 链接",
       children: [
         {
           text: "仿网易游戏",
--- docs/.vuepress/configs/sidebar.ts
+++ docs/.vuepress/configs/sidebar.ts
@@ -1,40 +1,152 @@
 export default {
+  "/guide/": [
+    {
+      text: "指南",
+      children: [
+        "/guide/index.md",
+        "/guide/start.md",
+      ],
+    },
+  ],
   "/JS/": [
     {
-      text: "开始",
+      text: "JavaScript 基础",
       children: [
         "/JS/基础/JS基础语法.md",
         "/JS/基础/分支语句.md",
         "/JS/基础/循环.md",
         "/JS/基础/数组.md",
-
         "/JS/基础/函数.md",
-
-        "/JS/面向对象/原型.md",
         "/JS/基础/事件对象.md",
         "/JS/基础/图片对象.md",
         "/JS/基础/正则验证.md",
         "/JS/基础/canvas.md",
-
-        "/JS/ES6/index.md",
       ],
     },
-    "/JS/算法/index.md",
+    {
+      text: "进阶与面向对象",
+      children: [
+        "/JS/ES6/index.md",
+        "/JS/面向对象/原型.md",
+        "/JS/算法/index.md",
+      ],
+    },
+    {
+      text: "移动端与小程序",
+      children: [
+        "/JS/PC/rem.md",
+        "/JS/微信小程序/index.md",
+        "/JS/微信小程序/share.md",
+        "/JS/uniapp/index.md",
+      ],
+    },
+  ],
+  "/vue/": [
+    {
+      text: "Vue 核心",
+      children: [
+        "/vue/vue/v-model.md",
+        "/vue/vue/filters.md",
+      ],
+    },
+    {
+      text: "Vue Router",
+      children: ["/vue/route/index.md"],
+    },
+    {
+      text: "Vuex 状态管理",
+      children: [
+        "/vue/vuex/index.md",
+        "/vue/vuex/state.md",
+        "/vue/vuex/getter.md",
+        "/vue/vuex/you.md",
+      ],
+    },
+    {
+      text: "扩展与插件",
+      children: [
+        "/vue/layout/index.md",
+        "/vue/插件/插件.md",
+        "/vue/插件/离线地图.md",
+        "/vue/插件/mock.md",
+      ],
+    },
+  ],
+  "/react/": [
+    {
+      text: "React",
+      children: ["/react/index.md"],
+    },
+  ],
+  "/scss/": [
+    {
+      text: "SCSS",
+      children: ["/scss/index.md"],
+    },
   ],
   "/Golang/": [
-    "/Golang/datatype/GO语言的主要特征.md",
-    "/Golang/datatype/数据结构.md",
-    "/Golang/datatype/datatype.md",
+    {
+      text: "Golang 基础",
+      children: [
+        "/Golang/datatype/hellow.md",
+        "/Golang/datatype/GO语言的主要特征.md",
+        "/Golang/datatype/datatype.md",
+        "/Golang/datatype/数据结构.md",
+        "/Golang/datatype/go包的引用.md",
+      ],
+    },
+    {
+      text: "数据库与进阶",
+      children: [
+        "/Golang/datasource/datasource.md",
+      ],
+    },
   ],
-  "/vue/vuex/": [
-    "/vue/vuex/index.md",
-    "/vue/vuex/state.md",
-    "/vue/vuex/getter.md",
+  "/rust/": [
+    {
+      text: "Rust",
+      children: ["/rust/index.md"],
+    },
   ],
-  "/vue/layout/": ["/vue/layout/index.md"],
-
-  "/scss/": ["/scss/index.md"],
-
-  "/fqa/npm/": ["/fqa/npm/npm.md"],
-  "/fqa/vue/": ["/fqa/vue/vue.md"],
+  "/computer/": [
+    {
+      text: "计算机基础",
+      children: ["/computer/计算机基础.md"],
+    },
+  ],
+  "/flutter/": [
+    {
+      text: "Flutter",
+      children: ["/flutter/flutter第一天.md"],
+    },
+  ],
+  "/THREE/": [
+    {
+      text: "Three.js",
+      children: [
+        "/THREE/模型加载.md",
+        "/THREE/射线检测.md",
+      ],
+    },
+  ],
+  "/emacs/": [
+    {
+      text: "Emacs 指南",
+      children: [
+        "/emacs/emacs基本操作.md",
+        "/emacs/doom基本操作.md",
+      ],
+    },
+  ],
+  "/fqa/": [
+    {
+      text: "面试与常见问题",
+      children: [
+        "/fqa/interview/前端面试复习计划.md",
+        "/fqa/vue/vue.md",
+        "/fqa/npm/npm.md",
+        "/fqa/git.md",
+      ],
+    },
+  ],
 };
